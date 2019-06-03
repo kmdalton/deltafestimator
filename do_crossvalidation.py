93e33d5978fd83535d64970cf87f9275817d650b
Kevin Dalton <kmdalton@pop-os.localdomain>
2019-06-03 18:52:05 -0400

    xval script modified to randomize entries

--- do_crossvalidation.py
+++ do_crossvalidation.py
@@ -4,15 +4,23 @@ import numpy as np
 from sys import argv
 
 
+#usage:
+# python do_crossvalidation.py input_file.csv output_file.csv [-c]
+# -c flag for control setting. this will ditch the 'off' data and 
+# replace it with one half of the 'on' data to serve as a negative control
+
+
 inFN = argv[1]
 outFN= argv[2]
-lmin,lmax,lstep = -5, 1, 50
+#lmin,lmax,lstep = -5, 1, 50
+lmin,lmax,lstep = -5, 1, 1
 lambdas = np.logspace(lmin, lmax, lstep)
 
 
 tolerance = 1e-7
 intensitykey = 'ipm2'
-maxiter = 2000
+#maxiter = 2000
+maxiter = 2
 referencekey = 'FCALC'
 
 
@@ -50,15 +58,47 @@ for k in I:
     else:
         del I[k]
 
+
 Ftrace1 = pd.DataFrame()
 Ftrace2 = pd.DataFrame()
 Icryst = pd.DataFrame()
 
-I['MASK'] = True
-I['MASK'] = np.array(I[['RUN', 'PHINUMBER', 'MASK']].groupby(['RUN', 'PHINUMBER']).transform(lambda x: np.random.random()) < 0.5)
+I['HALF'] = True
+I['HALF'] = np.array(I[['RUN', 'PHINUMBER', 'SERIES', 'HALF']].groupby(['RUN', 'PHINUMBER', 'SERIES']).transform(lambda x: np.random.random()) < 0.5)
+
+print(f"{len(I)} reflection observations before removing one third of the on data")
+#Either remove the off data and rename half the on data or...
+if len(argv) > 3 and argv[3] == '-c':
+    print("This is a control run -- deleting the off data")
+    from re import sub
+    I = I[I.SERIES.str.contains('on')]
+    I.loc[I.HALF, 'SERIES'] = I.loc[I.HALF, 'SERIES'].apply(lambda x: sub('on', 'off', x))
+#Ditch half of the on data so we are comparing apples to apples
+else:
+    I = I[I.SERIES.str.contains('off') | I.HALF]
+print(f"{len(I)} reflection observations after removing one third of the on data")
+
+I['on']  = I.SERIES.str.contains('on')
+I['off']  = I.SERIES.str.contains('off')
+I = I.groupby(['H', 'K', 'L', 'RUN', 'PHINUMBER']).filter(lambda x: x.on.max() and x.off.max())
+I['HALF'] = np.array(I[['RUN', 'PHINUMBER', 'HALF']].groupby(['RUN', 'PHINUMBER']).transform(lambda x: np.random.random()) < 0.5)
+print(f"{len(I)} reflection observations after paring incomplete phi numbers")
+print("""
+Data partitions
+Half:     1       2
+  on:  {}  {}
+ off:  {}  {}
+""".format(
+    (I.HALF  & I.SERIES.str.contains('on')).sum(),
+    (~I.HALF & I.SERIES.str.contains('on')).sum(),
+    (I.HALF  & I.SERIES.str.contains('off')).sum(),
+    (~I.HALF & I.SERIES.str.contains('off')).sum(),
+))
+print(f"{I.HALF.sum()} reflection observations in the on data set")
+print(f"{np.sum(~I.HALF)} reflection observations in the off data set")
 
 
-n = deltafestimator.deltafestimator_physical_gaussian(I[I['MASK']])
+n = deltafestimator.deltafestimator_physical_gaussian(I[I['HALF']])
 n.train({'LAMBDA': lambdas}, maxiter=maxiter, tolerance=tolerance)
 _Ftrace1 = n.result['Miller']
 _Icryst1 = n.result['Icryst']
@@ -68,7 +108,7 @@ Ftrace1 = pd.concat((Ftrace1, _Ftrace1))
 _Icryst1['HALF'] = 1
 Icryst = pd.concat((Icryst, _Icryst1))
 
-n = deltafestimator.deltafestimator_physical_gaussian(I[~I['MASK']])
+n = deltafestimator.deltafestimator_physical_gaussian(I[~I['HALF']])
 n.train({'LAMBDA': lambdas}, maxiter=maxiter, tolerance=tolerance)
 _Ftrace2 = n.result['Miller']
 _Icryst2 = n.result['Icryst']
